f4360db7f51509263b16c43bcffb90d199cf2cfd
kongbai <1039378419@qq.com>
2020-02-18 10:49:42 +0800

    idea提交github Test2

---
+++ Module02/src/com/atguigu/bean/GitTest2.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+/**
+ * @author shkstart
+ * @create 2020-02-18 10:48
+ */
+public class GitTest2 {
+    public static void main(String[] args) {
+        System.out.println("Test2");
+    }
+}
