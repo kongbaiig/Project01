211d3b668729f34e9f1b99eff8354faa19fa2189
kongbai <1039378419@qq.com>
2020-02-18 11:04:13 +0800

    idea提交github Test4

---
+++ Module02/src/com/atguigu/bean/GitTest05.java
@@ -0,0 +1,8 @@
+package com.atguigu.bean;
+
+/**
+ * @author shkstart
+ * @create 2020-02-18 10:54
+ */
+public class GitTest05 {
+}
---
+++ Module02/src/com/atguigu/bean/GitTest06.java
@@ -0,0 +1,8 @@
+package com.atguigu.bean;
+
+/**
+ * @author shkstart
+ * @create 2020-02-18 11:02
+ */
+public class GitTest06 {
+}
