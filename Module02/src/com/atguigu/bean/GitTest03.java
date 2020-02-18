2bbbc4d9258d96ed8b5077da6173ee1a12a3a949
kongbai <1039378419@qq.com>
2020-02-18 10:54:09 +0800

    idea提交github Test3

---
+++ Module02/src/com/atguigu/bean/GitTest03.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+/**
+ * @author shkstart
+ * @create 2020-02-18 10:52
+ */
+public class GitTest03 {
+    public static void main(String[] args) {
+        System.out.println("Test03");
+    }
+}
---
+++ Module02/src/com/atguigu/bean/GitTest04.java
@@ -0,0 +1,8 @@
+package com.atguigu.bean;
+
+/**
+ * @author shkstart
+ * @create 2020-02-18 10:53
+ */
+public class GitTest04 {
+}
